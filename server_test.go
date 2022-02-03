0dbe0e7773c8c018baf411a849a6603f4ce5ac4d
David Barringer <nukdcbear@gmail.com>
2022-02-03 14:50:37 -0500

    Fix build and add 2nd test

--- server_test.go
+++ server_test.go
@@ -25,3 +25,22 @@ func TestHello(t *testing.T) {
 		t.Errorf("expected hello got %v", string(data))
 	}
 }
+
+func TestHi(t *testing.T) {
+
+	req := httptest.NewRequest(http.MethodGet, "/hello", nil)
+	w := httptest.NewRecorder()
+
+	hi(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) != "Hi\n" {
+		t.Errorf("expected Hi got %v", string(data))
+	}
+}
